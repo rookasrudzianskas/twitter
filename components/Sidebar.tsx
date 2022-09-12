2d10590d0bf6c0211b3b4c9593379e22f8cb1a6c
Rokas <rookas.rudzenskas@gmail.com>
2022-09-12 16:52:18 +0300

    Rokas

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,16 +5,22 @@ import {
     BookmarkIcon,
     MapIcon,
     UserIcon,
-    HomeIcon
+    HomeIcon,
 } from "@heroicons/react/20/solid";
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import CollectionsIcon from '@mui/icons-material/Collections';
+import SidebarRow from "./SidebarRow";
 
 const Sidebar = () => {
     return (
         <div>
             <img src="https://links.papareact.com/drq" className="h-10 w-10" alt=""/>
-
+            <SidebarRow Icon={HomeIcon} title="Home" />
+            <SidebarRow Icon={HashtagIcon} title="Explore" />
+            <SidebarRow Icon={BellIcon} title="Notifications" />
+            <SidebarRow Icon={HomeIcon} title="Messages" />
+            <SidebarRow Icon={BookmarkIcon} title="Bookmarks" />
+            <SidebarRow Icon={MapIcon} title="Lists" />
         </div>
     );
 };
--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {SVGProps} from 'react';
 
-const SidebarRow = () => {
+interface Props {
+    Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+    title: string
+}
+
+const SidebarRow = ({Icon, title}: Props) => {
     return (
-        <div>
-            
+        <div className="flex max-w-fit items-center space-x-2 px-2 py-3 rounded-full hover:bg-gray-100 duration-150 cursor-pointer group">
+            <Icon className="h-6 w-6" />
+            <p className="group-hover:text-twitter">{title}</p>
         </div>
     );
 };
