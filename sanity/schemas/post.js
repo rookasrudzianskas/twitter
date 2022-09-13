ec0d919bc9b9bf489a2a4d00b12f6654a0f71243
Rokas <rookas.rudzenskas@gmail.com>
2022-09-13 09:09:07 +0300

    Rokas

--- sanity/schemas/post.js
+++ sanity/schemas/post.js
@@ -4,62 +4,30 @@ export default {
   type: 'document',
   fields: [
     {
-      name: 'title',
-      title: 'Title',
+      name: 'text',
+      title: 'Text in Tweet',
       type: 'string',
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      options: {
-        source: 'title',
-        maxLength: 96,
-      },
+      name: 'blockTweet',
+      title: 'Block Tweet',
+      description: 'ADMIN Controls: Toggle if Tweet is deemed inappropriate',
+        type: 'boolean',
     },
     {
-      name: 'author',
-      title: 'Author',
-      type: 'reference',
-      to: {type: 'author'},
+      name: 'username',
+      title: 'Username',
+      type: 'string',
     },
     {
-      name: 'mainImage',
-      title: 'Main image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
+      name: 'profileImg',
+      title: 'Profile Image',
+      type: 'string',
     },
     {
-      name: 'categories',
-      title: 'Categories',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
-    },
-    {
-      name: 'publishedAt',
-      title: 'Published at',
-      type: 'datetime',
-    },
-    {
-      name: 'body',
-      title: 'Body',
-      type: 'blockContent',
-    },
-  ],
-
-  preview: {
-    select: {
-      title: 'title',
-      author: 'author.name',
-      media: 'mainImage',
-    },
-    prepare(selection) {
-      const {author} = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      })
-    },
-  },
+      name: 'image',
+      title: "Tweet Image",
+      type: 'string',
+    }
+  ]
 }
