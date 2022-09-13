70247084f5aa675dd1e28ad28c69683f96e015e2
Rokas <rookas.rudzenskas@gmail.com>
2022-09-13 14:43:51 +0300

    Rokas

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -65,8 +65,9 @@ const Tweet = ({tweet}: Props) => {
             {comments?.length > 0 && (
                 <div className="my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5">
                     {comments.map(comment => (
-                        <div key={comment._id} className="flex space-x-2">
-                            <img src={comment.profileImg} className="h-7 w-7 rounded-full object-cover" alt=""/>
+                        <div key={comment._id} className="relative flex space-x-2">
+                            <hr className="absolute left-5 top-10 h-8 border-x border-twitter/30"/>
+                            <img src={comment.profileImg} className="mt-2 h-7 w-7 rounded-full object-cover" alt=""/>
                             <div className="">
                                 <div className="flex items-center space-x-1">
                                     <p className="mr-1 font-bold">{comment.username}</p>
