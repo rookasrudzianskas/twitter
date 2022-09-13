34ac5571acc1366eb07b6251d8fb8f9be6cca686
Rokas <rookas.rudzenskas@gmail.com>
2022-09-13 10:42:15 +0300

    Rokas

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import {Tweet} from '../typings';
 import TimeAgo from 'react-timeago'
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import CameraswitchIcon from '@mui/icons-material/Cameraswitch';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 interface Props {
     tweet: Tweet
@@ -15,7 +19,7 @@ const Tweet = ({tweet}: Props) => {
                 <div>
                     <div className="flex items-center space-x-1">
                         <p className="mr-1 font-bold">{tweet.username}</p>
-                        <p className="hidden text-sm text-gray-500 sm:inline">@{tweet.username.replace(/\s+/g, '').toLowerCase()}</p>
+                        <p className="hidden text-sm text-gray-500 sm:inline">@{tweet.username.replace(/\s+/g, '').toLowerCase()} • </p>
 
                         <TimeAgo date={tweet._createdAt} className="text-sm text-gray-500" />
                     </div>
@@ -24,6 +28,22 @@ const Tweet = ({tweet}: Props) => {
                     {tweet.image && (
                         <img src={tweet.image} className="m-5 ml-0 mb-1 max-h-60 rounded-lg object-cover shadow-sm" alt=""/>
                     )}
+                </div>
+            </div>
+
+            <div className="mt-5 flex justify-between">
+                <div className="flex cursor-pointer space-x-3 items-center text-gray-400">
+                    <ChatBubbleOutlineIcon className="w-5 h-5" />
+                    <p className="">2</p>
+                </div>
+                <div className="flex cursor-pointer space-x-3 items-center text-gray-400">
+                    <FavoriteBorderIcon className="w-5 h-5" />
+                </div>
+                <div className="flex cursor-pointer space-x-3 items-center text-gray-400">
+                    <CameraswitchIcon className="w-5 h-5" />
+                </div>
+                <div className="flex cursor-pointer space-x-3 items-center text-gray-400">
+                    <CloudUploadIcon className="w-5 h-5" />
                 </div>
             </div>
 
---
+++ utils/fetchComments.ts
@@ -0,0 +1,3 @@
+export const fetchComments = async (tweetId: string) => {
+
+}
